65b77d4c2734ba5836a254d3ec06d77638c6429c
Renan Lopes <rlopes@braspag.com.br>
2016-10-21 11:56:45 -0200

    Akka BootCamp Unit 1 - Lesson 1.3

---
+++ Akka/akka-bootcamp-master/src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -0,0 +1,51 @@
+using System;
+using Akka.Actor;
+
+namespace WinTail
+{
+    /// <summary>
+    /// Actor responsible for reading FROM the console. 
+    /// Also responsible for calling <see cref="ActorSystem.Terminate"/>.
+    /// </summary>
+    class ConsoleReaderActor : UntypedActor
+    {
+        public const string StartCommand = "start";
+        public const string ExitCommand = "exit";
+        private readonly IActorRef _validationActor;
+
+        public ConsoleReaderActor(IActorRef validationActor)
+        {
+            _validationActor = validationActor;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if (message.Equals(StartCommand))
+            {
+                DoPrintInstructions();
+            }
+
+            GetAndValidateInput();
+        }
+
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+            if (!string.IsNullOrEmpty(message)
+                && string.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                Context.System.Terminate();
+                return;
+            } 
+
+            _validationActor.Tell(message);
+        }
+
+        private void DoPrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
+    }
+}
---
+++ Akka/akka-bootcamp-master/src/Unit-1/DoThis/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+﻿using Akka.Actor;
+using Akka.Actor.Internal;
+
+namespace WinTail
+{
+    #region Program
+    class Program
+    {
+        public static ActorSystem MyActorSystem;
+
+        static void Main(string[] args)
+        {
+            // initialize MyActorSystem
+            MyActorSystem = ActorSystem.Create("MyActorSystem");
+            
+            var consoleWriterProps = Props.Create<ConsoleWriterActor>();
+            var consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps, "consoleWriterActor");
+
+            var validationActorProps = Props.Create(() => new ValidationActor(consoleWriterActor));
+            var validationActor = MyActorSystem.ActorOf(validationActorProps, "validationActor");
+
+            var consoleReaderProps = Props.Create<ConsoleReaderActor>(validationActor);
+            var consoleReaderActor = MyActorSystem.ActorOf(consoleReaderProps, "consoleReaderActor");
+
+            // tell console reader to begin
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
+
+            // blocks the main thread from exiting until the actor system is shut down
+            MyActorSystem.WhenTerminated.Wait();
+        }
+    }
+    #endregion
+}
